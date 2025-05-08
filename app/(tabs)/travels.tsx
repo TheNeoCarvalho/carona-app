611bf977586c8a925c01d5f2153e7cd9631c45b9
Manoel Carvalho <manoelmecat@gmail.com>
2025-05-08 16:25:45 -0300

    Travels list

--- app/(tabs)/travels.tsx
+++ app/(tabs)/travels.tsx
@@ -4,7 +4,18 @@ import React from 'react'
 const Travels = () => {
     return (
         <View>
-            <Text>Travels</Text>
+            <View>
+                <View>
+                    <Text>De: Cedro</Text>
+                    <Text>Para: Fortaleza</Text>
+                    <Text>Data: 20/09/2025</Text>
+                    <Text>Horário: 08:00h</Text>
+                </View>
+                <View>
+                    <Text>Vagas</Text>
+                    <Text>3/4</Text>
+                </View>
+            </View> 
         </View>
     )
 }
