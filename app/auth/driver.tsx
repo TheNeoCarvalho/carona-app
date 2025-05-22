f9a0fddac4ba3adf6de1e3266724c746a3524ef9
Manoel Carvalho <manoelmecat@gmail.com>
2025-05-22 15:38:09 -0300

    chore: adjusts driver screen

--- app/auth/driver.tsx
+++ app/auth/driver.tsx
@@ -1,13 +1,18 @@
-import { View, Text, TextInput, ScrollView, TouchableOpacity, Pressable, Alert } from 'react-native'
-import React, { useState } from 'react'
-import { MaterialIcons } from '@expo/vector-icons'
+import {
+  View,
+  Text,
+  TextInput,
+  ScrollView,
+  Pressable,
+  Alert,
+} from "react-native";
+import React, { useState } from "react";
+import { MaterialIcons } from "@expo/vector-icons";
 
-import { useRouter } from 'expo-router'
-import api from '../../service/api'
+import { useRouter } from "expo-router";
 
 const Driver = () => {
-
-  const router = useRouter()
+  const router = useRouter();
 
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
@@ -18,78 +23,62 @@ const Driver = () => {
   const [ano, setAno] = useState("");
   const [placa, setPlaca] = useState("");
 
-
   const handleSaveUserDriver = async () => {
-    console.log('name', name)
-    console.log('email', email)
-    console.log('password', password)
-    console.log('tel', tel)
-    console.log('marca', marca)
-    console.log('modelo', modelo)
-    console.log('ano', ano)
-    console.log('placa', placa)
+    console.log(marca);
+    console.log(modelo);
+    console.log(ano);
+    console.log(placa);
 
-
-    // const response = await api.post('register', {
-    //   name,
-    //   email,
-    //   password,
-    //   tel,
-    //   marca,
-    //   modelo,
-    //   ano,
-    //   placa
-    // })
-
-    // console.log(response)
-    
-    const response = await fetch('http://localhost:8000/api/register', {
-      method: 'POST',
+    const response = await fetch("http://localhost:8000/api/register", {
+      method: "POST",
       headers: {
-        'Content-Type': 'application/json',
+        "Content-Type": "application/json",
       },
-      
+
       body: JSON.stringify({
         name,
         email,
-        type: 'driver',
+        type: "driver",
         tel,
         password,
         marca,
         modelo,
         ano,
-        placa
+        placa,
       }),
-    })
-    const data = await response.json()
-    console.log(data)
+    });
+    const data = await response.json();
+    console.log(data);
     if (response.ok) {
-      Alert.alert('Cadastro realizado com sucesso!')
-      router.push('/(tabs)/home')
+      Alert.alert("Cadastro realizado com sucesso!");
+      router.push("/(tabs)/home");
     } else {
-      Alert.alert('Erro ao cadastrar motorista:', data)
+      Alert.alert("Erro ao cadastrar motorista:", data);
     }
-  }
+  };
 
   return (
     <>
-      <View style={{
-        flexDirection: 'row',
-        justifyContent: 'flex-start',
-        alignItems: 'center',
-        backgroundColor: '#000',
-        height: 86,
-        paddingLeft: 16,
-      }}>
+      <View
+        style={{
+          flexDirection: "row",
+          justifyContent: "flex-start",
+          alignItems: "center",
+          backgroundColor: "#000",
+          height: 86,
+          paddingLeft: 16,
+        }}
+      >
         <Pressable onPress={() => router.back()}>
-          <MaterialIcons name='arrow-back' color='#fff' size={24} />
+          <MaterialIcons name="arrow-back" color="#fff" size={24} />
         </Pressable>
         <Text
           style={{
             marginLeft: 16,
-            color: '#FFF',
+            color: "#FFF",
             fontSize: 22,
-          }}>
+          }}
+        >
           Cadastro de novo motorista
         </Text>
       </View>
@@ -98,7 +87,7 @@ const Driver = () => {
           style={{
             fontSize: 24,
             paddingHorizontal: 25,
-            paddingVertical: 18
+            paddingVertical: 18,
           }}
         >
           Vamos realizar o seu cadrastro, só precisamos de algumas informações
@@ -107,151 +96,166 @@ const Driver = () => {
           <Text
             style={{
               fontSize: 22,
-              fontWeight: 'bold',
-              marginBottom: 16
+              fontWeight: "bold",
+              marginBottom: 16,
             }}
-          >Informações Pessoais</Text>
+          >
+            Informações Pessoais
+          </Text>
           <Text style={{ fontSize: 18 }}>Nome Completo</Text>
           <TextInput
-            onChangeText={() => setName(name)}
+            onChangeText={(txt) => setName(txt)}
             value={name}
             style={{
-              backgroundColor: '#FDFDFD',
+              backgroundColor: "#FDFDFD",
               borderWidth: 1,
-              borderColor: '#C0C0C0',
+              borderColor: "#C0C0C0",
               borderRadius: 8,
               height: 42,
               marginBottom: 16,
-              padding: 8
-            }} />
+              padding: 8,
+            }}
+          />
           <Text style={{ fontSize: 18 }}>Email</Text>
-          <TextInput 
-            onChangeText={() => setEmail(email)}
-            value={email}  
+          <TextInput
+            onChangeText={(txt) => setEmail(txt)}
+            value={email}
             style={{
-              backgroundColor: '#FDFDFD',
+              backgroundColor: "#FDFDFD",
               borderWidth: 1,
-              borderColor: '#C0C0C0',
+              borderColor: "#C0C0C0",
               borderRadius: 8,
               height: 42,
               marginBottom: 16,
-              padding: 8
-            }} />
+              padding: 8,
+            }}
+          />
           <Text style={{ fontSize: 18 }}>Telefone/Whatsapp</Text>
-          <TextInput 
-            onChangeText={() => setTel(tel)}
+          <TextInput
+            onChangeText={(txt) => setTel(txt)}
             value={tel}
             style={{
-              backgroundColor: '#FDFDFD',
+              backgroundColor: "#FDFDFD",
               borderWidth: 1,
-              borderColor: '#C0C0C0',
+              borderColor: "#C0C0C0",
               borderRadius: 8,
               height: 42,
               marginBottom: 16,
-              padding: 8
-            }} />
+              padding: 8,
+            }}
+          />
           <Text style={{ fontSize: 18 }}>Senha</Text>
-          <TextInput 
-            onChangeText={() => setPassword(password)}
+          <TextInput
+            onChangeText={(txt) => setPassword(txt)}
             value={password}
             style={{
-              backgroundColor: '#FDFDFD',
+              backgroundColor: "#FDFDFD",
               borderWidth: 1,
-              borderColor: '#C0C0C0',
+              borderColor: "#C0C0C0",
               borderRadius: 8,
               height: 42,
               marginBottom: 16,
-              padding: 8
-            }} />
+              padding: 8,
+            }}
+          />
         </View>
         <View
           style={{
             paddingHorizontal: 25,
-            paddingVertical: 18
-          }}>
+            paddingVertical: 18,
+          }}
+        >
           <Text
             style={{
               fontSize: 22,
-              fontWeight: 'bold',
-              marginBottom: 16
+              fontWeight: "bold",
+              marginBottom: 16,
             }}
-          >Informações do Veículo</Text>
+          >
+            Informações do Veículo
+          </Text>
           <Text style={{ fontSize: 18 }}>Marca</Text>
-          <TextInput 
-            onChangeText={() => setMarca(marca)}
+          <TextInput
+            onChangeText={(txt) => setMarca(txt)}
             value={marca}
             style={{
-              backgroundColor: '#FDFDFD',
+              backgroundColor: "#FDFDFD",
               borderWidth: 1,
-              borderColor: '#C0C0C0',
+              borderColor: "#C0C0C0",
               borderRadius: 8,
               height: 42,
               marginBottom: 16,
-              padding: 8
-            }} />
+              padding: 8,
+            }}
+          />
           <Text style={{ fontSize: 18 }}>Modelo</Text>
-          <TextInput 
-            onChangeText={() => setModelo(modelo)}
+          <TextInput
+            onChangeText={(txt) => setModelo(txt)}
             value={modelo}
             style={{
-              backgroundColor: '#FDFDFD',
+              backgroundColor: "#FDFDFD",
               borderWidth: 1,
-              borderColor: '#C0C0C0',
+              borderColor: "#C0C0C0",
               borderRadius: 8,
               height: 42,
               marginBottom: 16,
-              padding: 8
-            }} />
+              padding: 8,
+            }}
+          />
           <Text style={{ fontSize: 18 }}>Ano</Text>
-          <TextInput 
-            onChangeText={() => setAno(ano)}
+          <TextInput
+            onChangeText={(txt) => setAno(txt)}
             value={ano}
             style={{
-              backgroundColor: '#FDFDFD',
+              backgroundColor: "#FDFDFD",
               borderWidth: 1,
-              borderColor: '#C0C0C0',
+              borderColor: "#C0C0C0",
               borderRadius: 8,
               height: 42,
               marginBottom: 16,
-              padding: 8
-            }} />
+              padding: 8,
+            }}
+          />
           <Text style={{ fontSize: 18 }}>Placa</Text>
-          <TextInput 
-            onChangeText={() => setPlaca(placa)}
+          <TextInput
+            onChangeText={(txt) => setPlaca(txt)}
             value={placa}
             style={{
-              backgroundColor: '#FDFDFD',
+              backgroundColor: "#FDFDFD",
               borderWidth: 1,
-              borderColor: '#C0C0C0',
+              borderColor: "#C0C0C0",
               borderRadius: 8,
               height: 42,
               marginBottom: 16,
-              padding: 8
-            }} />
+              padding: 8,
+            }}
+          />
         </View>
       </ScrollView>
-      <View style={{
-        backgroundColor: '#000',
-        height: 86,
-        justifyContent: 'center',
-        alignItems: 'center'
-      }}>
-        <Pressable 
+      <View
+        style={{
+          backgroundColor: "#000",
+          height: 86,
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <Pressable
           // onPress={() => router.push('/(tabs)/home')}
           onPress={handleSaveUserDriver}
         >
           <Text
             style={{
-              color: '#FFF',
+              color: "#FFF",
               fontSize: 22,
-            }}>
+            }}
+          >
             Cadastrar
           </Text>
         </Pressable>
       </View>
     </>
+  );
+};
 
-  )
-}
-
-export default Driver
+export default Driver;
